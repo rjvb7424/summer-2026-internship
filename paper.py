566062e50ee6621e0578884204790e5a4de377ea
rjvb7424 <rjvb7424@leeds.ac.uk>
2026-06-30 11:28:07 +0100

    Created a new generate_face function in the Paper class and made it so the paper updates the face after each fold

--- paper.py
+++ paper.py
@@ -7,30 +7,40 @@ class Paper():
         self.current_width = current_width
         self.current_height = current_height
         # 2D grid representing the current face of the paper
-        self.face = [[0 for _ in range(current_width)] for _ in range(current_height)]
+        self.face = self.generate_face()
         # History of folds made on the paper as a list of orientations
         self.fold_history = []
         # Number of layers of paper after folding
         self.layer = layer
 
+    def generate_face(self):
+        # generate_face was made into a function so that it can be called after each fold to update
+        return [[0 for _ in range(self.current_width)] for _ in range(self.current_height)]
+
     def fold(self, orientation = "north"):
+        # Fold the paper in the specified orientation
+        # Update the current dimensions, layer count,fold history and face accordingly
         match orientation:
             case "north":
                 self.current_height = self.current_height // 2
                 self.layer = self.layer * 2
                 self.fold_history.append("north")
+                self.face = self.generate_face()
             case "south":
                 self.current_height = self.current_height // 2
                 self.layer = self.layer * 2
                 self.fold_history.append("south")
+                self.face = self.generate_face()
             case "east":
                 self.current_width = self.current_width // 2
                 self.layer = self.layer * 2
                 self.fold_history.append("east")
+                self.face = self.generate_face()
             case "west":
                 self.current_width = self.current_width // 2
                 self.layer = self.layer * 2
                 self.fold_history.append("west")
+                self.face = self.generate_face()
 
     def visualize(self):
         # Print the current state of the paper's face
